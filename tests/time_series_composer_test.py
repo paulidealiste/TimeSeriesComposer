4972c8add846d108fb296ddba95d770b6afbeb8e
paulidealiste <paulidealiste@aol.com>
2019-01-22 13:36:49 +0100

    Multi series plot

--- tests/time_series_composer_test.py
+++ tests/time_series_composer_test.py
@@ -5,7 +5,7 @@ import os
 def test_tsc_read(capsys):
     tsc = time_series_composer.TimeSeriesComposer()
     basePath = os.path.dirname(os.path.abspath(__file__))
-    tsc.read_ts_data_json(basePath + "/tsdump.json")
+    tsc.read_ts_data_json(basePath + "/tssdump.json")
     out, err = capsys.readouterr()
     open(basePath + "/err.txt", "w").write(err)
     open(basePath + "/out.txt", "w").write(out)
--- time_series_composer/time_series_composer.py
+++ time_series_composer/time_series_composer.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -20,15 +21,22 @@ class TimeSeriesComposer(object):
         pass
 
     def read_ts_data_json(self, path):
-        ts = pd.read_json(path)
-        ts['Datetime'] = pd.to_datetime(ts['Datetime'])
-        ts = ts.set_index('Datetime')
-        print(ts)
-        self.plot_ts_data_png(ts['Data'])
+        with open(path) as timeseriesdata:
+            read_data = json.load(timeseriesdata)
+        timeserieslist = []
+        for series_id in read_data.keys():
+            ots = pd.DataFrame(read_data[series_id]['series_data'])
+            ots['date_time'] = pd.to_datetime(ots['date_time'])
+            ots = ots.set_index('date_time')
+            ots.columns = [series_id, series_id + 'pid']
+            timeserieslist.append(ots)
+        timeseriesframe = pd.concat(timeserieslist, axis=1)
+        print(timeseriesframe)
+        self.plot_ts_data_png(timeseriesframe[list(read_data.keys())])
 
     def plot_ts_data_png(self, ts):
         fig, ax = plt.subplots(figsize=(15, 7))
-        tsplot = ts.plot(ax=ax)
+        tsplot = ts.plot(ax=ax, marker="o")
         ax.xaxis.set_major_locator(mdates.HourLocator())
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %H:%M'))
         figure = tsplot.get_figure()
